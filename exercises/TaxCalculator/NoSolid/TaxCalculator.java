34ddcf75c88b327ee90fa19f93b95fbbe91a5bcc
juansefdz <jsfm81091@gmail.com>
2024-07-03 12:20:18 -0500

    exercise 6 ended

---
+++ exercises/TaxCalculator/NoSolid/TaxCalculator.java
@@ -0,0 +1,14 @@
+package exercises.TaxCalculator.NoSolid;
+
+public class TaxCalculator {
+    public double calculateTax(String country) {
+        if (country.equals("USA")) {
+            // Lógica para calcular el impuesto en USA
+            return 0.1;
+        } else if (country.equals("UK")) {
+            // Lógica para calcular el impuesto en UK
+            return 0.2;
+        }
+        return 0;
+    }
+}
